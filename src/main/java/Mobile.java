e089178eca551ed2d518ad96a94224f471fa84a3
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-10 12:28:08 +0800

    Updated

--- src/main/java/Mobile.java
+++ src/main/java/Mobile.java
@@ -4,10 +4,6 @@ public class Mobile {
     private String brand;
     private String message;
 
-    public mobile(String message){
-        this.message = message;
-    }
-
     public call(String message){
         System.out.println("Message: " + message);
     }
