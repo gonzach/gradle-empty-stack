2160efcd5e2d4a9bba000b3a48043f942e5c41a8
CHRISTINE GONZALES (SAPE-MIS-ISD-OOCLL/MNL) <GONZACH@OOCL.COM>
2019-10-10 13:45:11 +0800

    Story 2 mobile

--- src/main/java/Mobile.java
+++ src/main/java/Mobile.java
@@ -1,10 +1,46 @@
-public class Mobile {
+public abstract class Mobile {
     private String name;
     private String color;
     private String brand;
     private String message;
 
-    public call(String message){
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public void call(String message){
         System.out.println("Message: " + message);
     }
+
+    public void PrintInfo(){
+        System.out.print("name: "+ name + ", color: " + color + ", brand: " + brand);
+    }
+
+
 }
